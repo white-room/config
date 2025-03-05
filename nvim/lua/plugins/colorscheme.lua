ef82098ed9dc8feea9678e81cbed2e7ec92c63f5
Miles Dickson <miles@Miless-MacBook-Pro.local>
2025-03-04 22:53:07 -0700

    initial lazyvim config

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "catppuccin/nvim",
+    opts = {
+      flavour = "macchiato",
+    },
+  },
+
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "dark",
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
